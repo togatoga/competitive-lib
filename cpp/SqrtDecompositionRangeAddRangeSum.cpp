782634a1ede280d312b26151fb1b867c2b437ced
togatoga <togasakitogatoga@gmail.com>
2017-12-17 08:20:13 +0900

    Tweak SqrtDecompositionRangeAddRangeSum and verify it

--- cpp/SqrtDecompositionRangeAddRangeSum.cpp
+++ cpp/SqrtDecompositionRangeAddRangeSum.cpp
@@ -1,16 +1,23 @@
+//@verified http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=2649217#1
 #include <vector>
 using namespace std;
 
 //begin
-template<class T>
-class SqrtDecompositionRangeAddRangeSum {
+template <class T>
+class SqrtDecompositionRangeAddRangeSum
+{
+private:
   int N, K;
   int sqrtN;
   vector<T> data;
   vector<T> bucket_sum;
   vector<T> bucket_add;
-  SqrtDecompositionRangeAddRangeSum(int n) : N(n) {
-    while (sqrtN * sqrtN <= N){
+
+public:
+  SqrtDecompositionRangeAddRangeSum(int n) : N(n), K(0), sqrtN(1)
+  {
+    while (sqrtN * sqrtN <= N)
+    {
       sqrtN++;
     }
     K = (N + sqrtN - 1) / sqrtN;
@@ -19,17 +26,23 @@ class SqrtDecompositionRangeAddRangeSum {
     bucket_add.assign(K, 0);
   }
   // [s, t)
-  void add(int s, int t, T x) {
+  void add(int s, int t, T x)
+  {
     int p = s / sqrtN;
-    int q = q / sqrtN;
-    for (int k = p; k <= q; ++k) {
+    int q = t / sqrtN;
+    for (int k = p; k <= q; ++k)
+    {
       int l = k * sqrtN, r = (k + 1) * sqrtN;
       if (r <= s || t <= l)
         continue;
-      if (s <= l && r <= t) {
+      if (s <= l && r <= t)
+      {
         bucket_add[k] += x;
-      } else {
-        for (int i = max(s, l); i < min(t, r); ++i) {
+      }
+      else
+      {
+        for (int i = max(s, l); i < min(t, r); ++i)
+        {
           data[i] += x;
           bucket_sum[k] += x;
         }
@@ -37,25 +50,32 @@ class SqrtDecompositionRangeAddRangeSum {
     }
   }
   // [s, t)
-  T get_sum(int s, int t) {
+  T get_sum(int s, int t)
+  {
     int p = s / sqrtN;
-    int q = q / sqrtN;
+    int q = t / sqrtN;
     T sum = 0;
-    for (int k = p; k <= q; ++k) {
+    for (int k = p; k <= q; ++k)
+    {
       int l = k * sqrtN, r = (k + 1) * sqrtN;
       if (r <= s || t <= l)
         continue;
-      if (s <= l && r <= t) {
+      if (s <= l && r <= t)
+      {
         sum += bucket_sum[k] + bucket_add[k] * sqrtN;
-      } else {
-        for (int i = max(s, l); i < min(t, r); ++i) {
+      }
+      else
+      {
+        for (int i = max(s, l); i < min(t, r); ++i)
+        {
           sum += data[i] + bucket_add[k];
         }
       }
     }
     return sum;
   }
-  T get_value(int i){
+  T get_value(int i)
+  {
     int k = i / sqrtN;
     return data[i] + bucket_add[k];
   }
