caaae04233bd409391e275e09e9d8230c3eeaf43
togatoga <togasakitogatoga@gmail.com>
2019-03-23 19:06:15 +0900

    Add read_line to template.rs

--- rust/template.rs
+++ rust/template.rs
@@ -11,8 +11,7 @@ pub struct Scanner<R> {
 impl<R: std::io::Read> Scanner<R> {
     pub fn read<T: std::str::FromStr>(&mut self) -> T {
         use std::io::Read;
-        let buf = self
-            .stdin
+        let buf = self.stdin
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
@@ -24,9 +23,24 @@ impl<R: std::io::Read> Scanner<R> {
             .ok()
             .expect("Parse error.")
     }
+    pub fn read_line(&mut self) -> String {
+        use std::io::Read;
+        let buf = self.stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
     pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
         (0..n).map(|_| self.read()).collect()
     }
+
     pub fn chars(&mut self) -> Vec<char> {
         self.read::<String>().chars().collect()
     }
