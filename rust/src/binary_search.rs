7611749331bb879eddb1c46a059d5166fb0ab014
togatoga <togasakitogatoga@gmail.com>
2019-11-30 10:56:46 +0900

    Add binary_search.rs

--- rust/src/binary_search.rs
+++ rust/src/binary_search.rs
@@ -12,7 +12,7 @@ impl<T: Ord> BinarySearch<T> for [T] {
         let mut right = self.len();
         let mut result = None;
         while left < right {
-            let mut med = (low + right) / 2;
+            let med = (left + right) / 2;
             match self[med].cmp(x) {
                 Ordering::Less => {
                     left = med + 1;
@@ -30,7 +30,7 @@ impl<T: Ord> BinarySearch<T> for [T] {
         let mut right = self.len();
         let mut result = None;
         while left < right {
-            let mut med = (low + right) / 2;
+            let med = (left + right) / 2;
             match self[med].cmp(x) {
                 Ordering::Equal | Ordering::Less => {
                     left = med + 1;
@@ -47,9 +47,53 @@ impl<T: Ord> BinarySearch<T> for [T] {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use rand::{thread_rng, Rng};
     #[test]
     fn test_binary_search() {
         let vec = vec![-100, 0, 1, 2, 10, 100, 1000];
         assert_eq!(vec.lower_bound(&1), Some(2));
+        assert_eq!(vec.lower_bound(&100), Some(5));
+        assert_eq!(vec.lower_bound(&-5000), Some(0));
+        assert_eq!(vec.lower_bound(&999), Some(6));
+        assert_eq!(vec.lower_bound(&1001), None);
+
+        assert_eq!(vec.upper_bound(&-101), Some(0));
+        assert_eq!(vec.upper_bound(&0), Some(2));
+        assert_eq!(vec.upper_bound(&100), Some(6));
+        assert_eq!(vec.upper_bound(&1000), None);
+    }
+
+    #[test]
+    fn test_random_binary_search() {
+        let test_case = 10;
+        let max_value = 100;
+        let query_num = 100;
+        let mut rng = thread_rng();
+        for _ in 0..test_case {
+            let mut seq: Vec<i32> = (0..100).map(|_| rng.gen_range(0, max_value)).collect();
+            seq.sort();
+
+            for _ in 0..query_num {
+                let query = rng.gen_range(0, max_value);
+                let result = (seq.lower_bound(&query), seq.upper_bound(&query));
+                //sequential search
+                let naive_search = |x: i32| {
+                    let mut lower_result = None;
+                    let mut upper_result = None;
+                    for i in 0..seq.len() {
+                        if lower_result.is_none() && x <= seq[i] {
+                            lower_result = Some(i);
+                        }
+                        if upper_result.is_none() && x < seq[i] {
+                            upper_result = Some(i);
+                        }
+                    }
+                    (lower_result, upper_result)
+                };
+
+                assert_eq!(result, naive_search(query));
+            }
+        }
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod binary_search;
 pub mod mod_int;
 pub mod mod_util;
 pub mod rolling_hash;
