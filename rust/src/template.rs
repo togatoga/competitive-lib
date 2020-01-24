7bbaa13c33070bc516a1c93ebcc253a235139006
togatoga <togasakitogatoga@gmail.com>
2020-01-24 12:37:17 +0900

    Add utils

--- rust/src/template.rs
+++ rust/src/template.rs
@@ -11,6 +11,34 @@ fn main() {
         .join()
         .unwrap();
 }
+pub mod utils {
+    const DYX: [(isize, isize); 8] = [
+        (0, 1),   //right
+        (1, 0),   //down
+        (0, -1),  //left
+        (-1, 0),  //top
+        (1, 1),   //down right
+        (-1, 1),  //top right
+        (1, -1),  //down left
+        (-1, -1), //top left
+    ];
+
+    pub fn try_adj(y: usize, x: usize, dir: usize, h: usize, w: usize) -> Option<(usize, usize)> {
+        let ny = y as isize + DYX[dir].0;
+        let nx = x as isize + DYX[dir].1;
+        if ny >= 0 && nx >= 0 {
+            let ny = ny as usize;
+            let nx = nx as usize;
+            if ny < h && nx < w {
+                Some((ny, nx))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
 
 //snippet from kenkoooo
 pub struct Scanner<R> {
@@ -20,7 +48,8 @@ pub struct Scanner<R> {
 impl<R: std::io::Read> Scanner<R> {
     pub fn read<T: std::str::FromStr>(&mut self) -> T {
         use std::io::Read;
-        let buf = self.stdin
+        let buf = self
+            .stdin
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
@@ -34,7 +63,8 @@ impl<R: std::io::Read> Scanner<R> {
     }
     pub fn read_line(&mut self) -> String {
         use std::io::Read;
-        let buf = self.stdin
+        let buf = self
+            .stdin
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
