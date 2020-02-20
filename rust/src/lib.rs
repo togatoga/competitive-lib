04b4b4dbd99724c765da637fed951db80339953d
togatoga <togasakitogatoga@gmail.com>
2020-02-20 16:36:16 +0900

    Add timer.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,4 @@ pub mod mod_int;
 pub mod mod_util;
 pub mod rolling_hash;
 pub mod fenwick_tree;
+pub mod timer;
---
+++ rust/src/timer.rs
@@ -0,0 +1,41 @@
+pub mod timer {
+    use std::time::{Duration, Instant};
+    pub struct Timer {
+        start: Option<Instant>,
+    }
+    impl Timer {
+        pub fn new() -> Timer {
+            Timer { start: None }
+        }
+        pub fn reset(&mut self) {
+            self.start = None;
+        }
+        pub fn start(&mut self) {
+            self.start = Some(Instant::now());
+        }
+        pub fn elapsed(&self) -> Option<Duration> {
+            if let Some(start) = self.start {
+                Some(Instant::now() - start)
+            } else {
+                None
+            }
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::timer::Timer;
+    use std::time::Duration;
+    #[test]
+    fn test_timer() {
+        let mut timer = Timer::new();
+        timer.start();
+        //sleep 200 millis
+        std::thread::sleep(Duration::from_millis(200));
+        let elapsed = timer.elapsed().unwrap();
+        assert!(elapsed <= Duration::from_millis(210));
+
+        timer.reset();
+        assert_eq!(timer.elapsed(), None);
+    }
+}
