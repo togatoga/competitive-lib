3a8ccd51f5655d6a8409ae8d6c5c96d8c29f7907
togatoga <togasakitogatoga@gmail.com>
2020-10-03 21:25:27 +0900

    WIP impl

--- rust/src/lazy_segment_tree.rs
+++ rust/src/lazy_segment_tree.rs
@@ -11,13 +11,72 @@ pub mod lazy_segment_tree {
         fn identity_element() -> <Self::M as Monoid>::S {
             Self::M::identity()
         }
-        
-        fn binary_operation(a: &<Self::M as Monoid>::S, b: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
+
+        fn binary_operation(
+            a: &<Self::M as Monoid>::S,
+            b: &<Self::M as Monoid>::S,
+        ) -> <Self::M as Monoid>::S {
             Self::M::binary_operation(a, b)
         }
 
         fn identity_map() -> Self::F;
         fn mapping(f: &Self::F, x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S;
         fn composition(f: &Self::F, g: &Self::F) -> Self::F;
+    }
+
+    pub struct LazySegMentTree<F>
+    where
+        F: MapMonid,
+    {
+        size: usize,
+        height: usize,
+        data: Vec<<F::M as Monoid>::S>,
+        lazy: Vec<F::F>,
+    }
+    impl<F> LazySegMentTree<F>
+    where
+        F: MapMonid,
+    {
+        pub fn new(n: usize) -> Self {
+            let mut size = 1;
+            let mut height = 0;
+            while size < n {
+                size <<= 1;
+                height += 1;
+            }
+            let data = vec![F::identity_element(); 2 * size];
+            let lazy = vec![F::identity_map(); 2 * size];
+            LazySegMentTree {
+                size,
+                height,
+                data,
+                lazy,
+            }
+        }
+        pub fn set(&mut self, p: usize, x: <F::M as Monoid>::S) {
+            self.data[p + self.size] = x;
+        }
+
+        fn eval(&mut self, k: usize) {
+            if k < self.size {
+                self.lazy[2 * k] = F::composition(&self.lazy[2 * k], &self.lazy[k]);
+                self.lazy[2 * k + 1] = F::composition(&self.lazy[2 * k + 1], &self.lazy[k]);
+            }
+            self.data[k] = F::mapping(&self.lazy[k], &self.data[k]);
+        }
+        fn update(&mut self, a: usize, b: usize, f: F::F, k: usize, l: usize, r: usize) {
+            self.eval(k);
+            if a <= l && r <= b {
+                self.lazy[k] = F::composition(&self.lazy[k], &f);
+                self.eval(k);
+            } else if a < r && l < b {
+                self.update(a, b, f.clone(), 2 * k, l, (l + r) >> 1);
+                self.update(a, b, f.clone(), 2 * k + 1, (l + r) >> 1, r);
+                self.data[k] = F::binary_operation(&self.data[2 * k], &self.data[2 * k + 1]);
+            }
+        }
+        pub fn apply(&mut self, a: usize, b: usize, f: F::F) {
+            self.update(a, b, f, 1, 0, self.size);
+        }
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod binary_search;
 pub mod fenwick_tree;
 pub mod gcd;
+pub mod lazy_segment_tree;
 pub mod mod_int;
 pub mod mod_util;
 pub mod rolling_hash;
