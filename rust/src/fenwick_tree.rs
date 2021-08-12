0f248051ff2db7114077ed92dce1b4d3c57abc71
togatoga <togasakitogatoga@gmail.com>
2021-08-12 10:49:27 +0900

    Add sub function for fenwick_tree.rs

--- rust/src/fenwick_tree.rs
+++ rust/src/fenwick_tree.rs
@@ -8,18 +8,18 @@ pub mod fenwick_tree {
         init_value: T,
     }
 
-    impl<T: Copy + Clone + AddAssign + Sub<Output = T>> FenwickTree<T> {
+    impl<T: Copy + Clone + AddAssign + SubAssign + Sub<Output = T>> FenwickTree<T> {
         pub fn new(n: usize, init_value: T) -> FenwickTree<T> {
             FenwickTree {
                 values: vec![init_value; n + 1],
                 init_value,
             }
         }
-        //[l, r)
+        /// Caluculate the range sum [l, r)
         pub fn query(&self, l: usize, r: usize) -> T {
             self.sum(r) - self.sum(l)
         }
-        //[0, i)
+        /// Caluculate the range sum [0, i)
         pub fn sum(&self, i: usize) -> T {
             let mut result = self.init_value;
             let mut x = i as i32 - 1;
@@ -30,10 +30,20 @@ pub mod fenwick_tree {
             result
         }
 
+        /// Add a[i] += x
         pub fn add(&mut self, i: usize, x: T) {
             let mut index = i;
             while index < self.values.len() {
                 self.values[index] += x;
+                index |= index + 1;
+            }
+        }
+
+        /// Sub a[i] -= x
+        pub fn sub(&mut self, i: usize, x: T) {
+            let mut index = i;
+            while index < self.values.len() {
+                self.values[index] -= x;
                 index |= index + 1;
             }
         }
@@ -48,11 +58,16 @@ mod test {
         let n = 1000;
         let mut bit = FenwickTree::new(n, 0);
         let mut v = vec![0; n];
-        for _ in 0..10000 {
+        for i in 0..10000 {
             let value = thread_rng().gen_range(0, 1000);
             let k = thread_rng().gen_range(0, n);
-            v[k] += value;
-            bit.add(k, value);
+            if i % 2 == 0 {
+                v[k] += value;
+                bit.add(k, value);
+            } else {
+                v[k] -= value;
+                bit.sub(k, value);
+            }
 
             let mut sum = 0;
             for i in 0..n {
