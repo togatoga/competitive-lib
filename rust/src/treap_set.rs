a27c93db09f5474ba7090934cf475daccc357b50
togatoga <togasakitogatoga@gmail.com>
2021-12-11 01:47:59 +0900

    WIP

--- rust/src/treap_set.rs
+++ rust/src/treap_set.rs
@@ -1,4 +1,5 @@
 pub mod treap_set {
+    use std::cmp::Ordering;
 
     #[derive(Debug, Clone)]
     struct Node<T> {
@@ -8,7 +9,7 @@ pub mod treap_set {
         priority: u32,
         size: usize,
     }
-    impl<T: PartialOrd> Node<T> {
+    impl<T: Ord> Node<T> {
         fn new(key: T, priority: u32) -> Node<T> {
             Node {
                 key,
@@ -18,27 +19,39 @@ pub mod treap_set {
                 size: 1,
             }
         }
-        fn size(&self) -> usize {
-            self.size
-        }
         fn update_size(&mut self) {
             self.size = size(&self.left) + size(&self.right) + 1;
         }
-
         fn find(&self, value: &T) -> (bool, usize) {
             let left_size = size(&self.left);
             match self.key.cmp(value) {
-                _ => {}    
+                Ordering::Equal => (true, left_size),
+                Ordering::Greater => self
+                    .left
+                    .as_ref()
+                    .map_or((false, 0), |left| left.find(value)),
+                Ordering::Less => self.right.as_ref().map_or((false, left_size + 1), |right| {
+                    let (contained, size) = right.find(value);
+                    (contained, size + left_size + 1)
+                }),
             }
-            todo!()
+        }
+        fn kth(&self, k: usize) -> Option<&T> {
+            let left_size = size(&self.left);
+            if left_size > k {
+                self.left.as_ref().map_or(None, |left| left.kth(k))
+            } else if left_size == k {
+                Some(&self.key)
+            } else {
+                self.right
+                    .as_ref()
+                    .map_or(None, |right| right.kth(k - left_size - 1))
+            }
         }
     }
 
-
-
-
     fn size<T>(node: &Option<Box<Node<T>>>) -> usize {
-        node.as_ref().map_or(0, |node| node.size())
+        node.as_ref().map_or(0, |node| node.size)
     }
 
     #[derive(Debug, Clone)]
@@ -56,7 +69,7 @@ pub mod treap_set {
         }
     }
 
-    impl<T> TreapSet<T> {
+    impl<T: Ord> TreapSet<T> {
         /// Make a new empty `TreapSet`
         pub fn new(seed: u32) -> TreapSet<T> {
             TreapSet {
@@ -68,6 +81,78 @@ pub mod treap_set {
         /// Returns the number of elements in the tree
         pub fn len(&self) -> usize {
             size(&self.root)
+        }
+
+        pub fn insert(&mut self, value: T) -> bool {
+            let priority = self.rng.next();
+            if let Some(root) = self.root.take() {
+                let (contained, k) = root.find(&value);
+                if !contained {
+                    self.root = Some(insert(Some(root), k, value, priority));
+                    true
+                } else {
+                    self.root = Some(root);
+                    false
+                }
+            } else {
+                self.root = Some(Box::new(Node::new(value, priority)));
+                true
+            }
+        }
+        pub fn kth(&self, k: usize) -> Option<&T> {
+            self.root.as_ref().map_or(None, |root| root.kth(k))
+        }
+    }
+
+    fn insert<T: Ord>(node: Option<Box<Node<T>>>, k: usize, key: T, priority: u32) -> Box<Node<T>> {
+        let (left, right) = split(node, k);
+        let node = merge(left, Some(Box::new(Node::new(key, priority))));
+        let mut node = merge(node, right).expect("empty node");
+        node.update_size();
+        node
+    }
+
+    fn merge<T: Ord>(
+        left: Option<Box<Node<T>>>,
+        right: Option<Box<Node<T>>>,
+    ) -> Option<Box<Node<T>>> {
+        match (left, right) {
+            (Some(mut left), Some(mut right)) => {
+                if left.priority > right.priority {
+                    left.right = merge(left.right, Some(right));
+                    left.update_size();
+                    Some(left)
+                } else {
+                    right.left = merge(Some(left), right.left);
+                    right.update_size();
+                    Some(right)
+                }
+            }
+            (Some(left), None) => Some(left),
+            (None, Some(right)) => Some(right),
+            (None, None) => None,
+        }
+    }
+
+    fn split<T: Ord>(
+        node: Option<Box<Node<T>>>,
+        k: usize,
+    ) -> (Option<Box<Node<T>>>, Option<Box<Node<T>>>) {
+        if let Some(mut node) = node {
+            let left_size = size(&node.left);
+            if k <= left_size {
+                let (left, right) = split(node.left.take(), k);
+                node.left = right;
+                node.update_size();
+                (left, Some(node))
+            } else {
+                let (left, right) = split(node.right.take(), k - left_size - 1);
+                node.right = left;
+                node.update_size();
+                (Some(node), right)
+            }
+        } else {
+            (None, None)
         }
     }
 
