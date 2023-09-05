fb14b5cafd5c0d8a08918c0c88d6602effd112ce
togatoga <hitoshitogasaki@gmail.com>
2023-09-05 23:13:06 +0900

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,13 @@
 use cargo_snippet::snippet;
 use rust::fastio::fastio;
 #[snippet(name = "solver", include = "fastio")]
-#[derive(Default)]
-pub struct Solver {}
-#[snippet(name = "solver")]
-impl Solver {
-    pub fn solve(&mut self) {
-        let stdin = std::io::stdin();
-        #[allow(unused_mut, unused_variables)]
-        let mut scn = fastio::Scanner::new(stdin.lock());
-    }
+fn run() {    
+    #[allow(unused_mut, unused_variables)]
+    let mut scn = fastio::Scanner::new(std::io::stdin().lock());
 }
 
 #[snippet(include = "macros")]
 #[snippet(include = "solver")]
 fn main() {
-    std::thread::Builder::new()
-        .stack_size(256 * 1024 * 1024) // 256MB
-        .spawn(|| Solver::default().solve())
-        .unwrap()
-        .join()
-        .unwrap();
+    run()
 }
