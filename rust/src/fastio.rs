760d0d32db36397bd1123591e0bc04c358571c37
togatoga <hitoshitogasaki@gmail.com>
2023-09-05 23:04:26 +0900

    Update fastio.rs

--- rust/src/fastio.rs
+++ rust/src/fastio.rs
@@ -5,7 +5,6 @@ pub mod fastio {
     use std::collections::VecDeque;
     use std::io::BufWriter;
     use std::io::Write;
-    use std::ops::Sub;
 
     pub struct Writer<W: std::io::Write> {
         writer: std::io::BufWriter<W>,
@@ -29,12 +28,10 @@ pub mod fastio {
             self.write('\n');
         }
     }
-
     pub struct Scanner<R> {
         stdin: R,
         buffer: VecDeque<String>,
     }
-
     impl<R: std::io::BufRead> Scanner<R> {
         pub fn new(s: R) -> Scanner<R> {
             Scanner {
@@ -51,27 +48,29 @@ pub mod fastio {
             }
             self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
         }
-        pub fn read1<T: std::str::FromStr + Sub<Output = T> + From<u8>>(&mut self) -> T {
-            self.read::<T>() - T::from(1)
+
+        pub fn usize1(&mut self) -> usize {
+            self.read::<usize>() - 1
         }
+
+        pub fn tuple<T1: std::str::FromStr, T2: std::str::FromStr>(&mut self) -> (T1, T2) {
+            (self.read::<T1>(), self.read::<T2>())
+        }
+
         pub fn read_line(&mut self) -> String {
             let mut line = String::new();
             let _ = self.stdin.read_line(&mut line);
             line.trim_end().to_string()
         }
-
         pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
             (0..n).map(|_| self.read()).collect()
         }
-
         pub fn vchars(&mut self, n: usize) -> Vec<Vec<char>> {
             (0..n).map(|_| self.chars()).collect::<Vec<Vec<char>>>()
         }
-
         pub fn vvec<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
             (0..n).map(|_| self.vec::<T>(m)).collect::<Vec<Vec<T>>>()
         }
-
         pub fn chars(&mut self) -> Vec<char> {
             self.read::<String>().chars().collect()
         }
